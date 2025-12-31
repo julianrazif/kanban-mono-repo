846094c179cba57e4ecab304cb3202b240612bee
JULIAN RAZIF FIGARO <julian.razif@gmail.com>
2025-12-31 18:52:40 +0700

    Renames the core application and test classes, and updates the Spring application name for consistency.

--- apps/servers/spring-boot/src/test/java/com/julian/razif/kanban/KanbanApplicationTests.java
+++ apps/servers/spring-boot/src/test/java/com/julian/razif/kanban/KanbanApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KanbanServerSbApplicationTests {
+class KanbanApplicationTests {
 
   @Test
   void contextLoads() {
